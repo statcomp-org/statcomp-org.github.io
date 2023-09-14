01388b464adc0fffd92443c317520ddd82c9140c
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2023-09-14 13:44:32 +0000

    Built site for gh-pages

--- lua-refs.lua
+++
@@ -1,8 +0,0 @@
-function Pandoc (doc)
-  local references = pandoc.utils.references(doc)
-  doc.blocks = pandoc.Blocks{
-    pandoc.Header(1, "Publications"),
-    pandoc.Div({}, "list")
-  }
-  return pandoc.utils.citeproc(doc)
-end
